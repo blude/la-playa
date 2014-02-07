7ccabdfa6c743e859c8461b51500cf583bdddb72
Saulo Pratti <sppratti@gmail.com>
2014-02-07 03:16:11 -0200

    Funcionalidade básica adicionada

--- js/main.js
+++ js/main.js
@@ -1,30 +1,105 @@
 var App = (function() {
-	var map, lc;
-	return {
-		locateSelf: function() {
-			var options = {
-				follow: true,
-				onLocationError: function(err) {
-					alert('Não consegui encontrar sua localização');
-				},
-				onLocationOutsideMapBounds: function(context) {
-					alert(context.options.strings.outsideMapBoundsMsg);
-				},
-				strings: {
-					outsideMapBoundsMsg: "Você não está perto de uma praia monitorada"
-				}
-			};
-			lc = L.control.locate(options).addTo(map);
-			lc.locate();
-		},
-		initMap: function() {
-			map = L.mapbox.map('map', 'examples.map-9ijuk24y');
-			map.setView([-20.2576356,-40.2835713], 13);
-			this.locateSelf();
-		}
-	};
+    var map, self = this;
+
+    function setSelfLocation(e) {
+        var marker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
+        self.currentLatLng = marker.getLatLng();
+    }
+
+    function getDataUrl(file) {
+        return 'https://api.github.com/repos/vitoria-es/qualidade-das-praias/contents/data/<FILE>'.replace('<FILE>', file);
+    }
+
+    function loadPlayas() {
+        var URL = getDataUrl('balneabilidade.geojson');
+
+        $.ajax({
+            headers: {
+                'Accept': 'application/vnd.github.v3.raw'
+            },
+            dataType: 'json',
+            url: URL,
+            success: function(geodata) {
+                var playas = L.mapbox.featureLayer(geodata).addTo(map);
+                self.playas = playas.toGeoJSON();
+                self.closestPlaya = getClosestPlaya();
+                if (isPlayaTooFar(self.closestPlaya.distance)) {
+                    alert('Tem certeza que tu tá na praia, brother?');
+                }
+            }
+        });
+    }
+
+    function loadHistory() {
+        var URL = getDataUrl('historico.json');
+
+        $.ajax({
+            headers: {
+                'Accept': 'application/vnd.github.v3.raw'
+            },
+            dataType: 'json',
+            url: URL,
+            success: function(response) {
+                self.historyData = response.data;
+            }
+        });
+    }
+
+    function isUpToDate(analysis) {
+        var now = new Date();
+        return analysis['valid_until'] < now;
+    }
+
+    function isPlayaTooFar(distance) {
+        var THRESHOLD = 100; // max distance in meters to a playa for it to be considered
+        return distance > THRESHOLD;
+    }
+
+    function getLastAnalysis() {
+        return self.historyData[0];
+    }
+
+    function getClosestPlaya() {
+        var closest = -1,
+            distances = [];
+
+        for (var i = 0, length = self.playas.features.length; i < length; i++) {
+            var playaCoords = self.playas.features[i].geometry.coordinates,
+                playaLatLng = L.marker([playaCoords[1], playaCoords[0]]).getLatLng(),
+                distanceToPlaya = self.currentLatLng.distanceTo(playaLatLng);
+            distances[i] = distanceToPlaya;
+            if (closest === -1 || distanceToPlaya < distances[closest]) {
+                closest = i;
+            }
+        }
+
+        return {
+            name: self.playas.features[closest].properties['nome'],
+            distance: distances[closest]
+        };
+    }
+
+    function bindEvents() {
+        map.on('locationfound', function(e) {
+            setSelfLocation(e);
+            loadHistory();
+//          getLastAnalysis();
+            loadPlayas();
+        });
+        map.on('locationerror', function() {
+            alert('Não foi possível encontrar sua localização.');
+        });
+    }
+
+    return {
+        initMap: function() {
+            map = L.mapbox.map('map', 'examples.map-9ijuk24y');
+            map.locate({ setView: true });
+            bindEvents();
+        }
+    };
 }());
 
 (function() {
-	App.initMap();
+    App.initMap();
 })();
